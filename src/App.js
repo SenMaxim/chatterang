ce0d8970704903e042b97f07ecc946283cf39439
Maxim Sen <maxsen06@gmail.com>
2018-06-12 22:34:56 -0400

    added a set field to user to determine which div to return

--- src/App.js
+++ src/App.js
@@ -9,21 +9,28 @@ class App extends Component {
     user: {
       uid: '123',
       userName: 'chris',
+      set: true
     }
   }
 
 
   updateUser = (newUser) => {
-    this.setState({user: newUser})
+    this.setState({user: newUser, set: true})
   }
 
 
 
   render() {
+    if (this.state.set) {
+      return (
+        <div className="App">
+            <Main user={this.state.user}/>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-          <Main user={this.state.user}/>
-
           <SignIn updateUser={this.updateUser}/>
       </div>
     );
